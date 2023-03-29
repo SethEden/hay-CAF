922a0a415e161cf74e68d9c84d4e49bf49a923e7
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-03-29 18:47:56 -0500

    projStart: Establish the main entry point file for the application.

---
+++ src/hay-CAF.js
@@ -0,0 +1,15 @@
+/**
+ * @file hay-CAF.js
+ * @module hay-CAF
+ * @description This is the main nit for the hay-CAF Haystacks Cloud Automation Framework application.
+ * It contains just enough of the main program loop and basic argument parsing to function as an
+ * interactive enterprise testing automation execution framework. Based and build using the Haystacks platform.
+ * @requires {@link https://www.npmjs.com/package/@haystacks/async|@haystacks/async}
+ * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
+ * @requires {@link https://www.npmjs.com/package/url|url}
+ * @requires {@link https://www.npmjs.com/package/dotenv|dotenv}
+ * @requires {@link https://www.npmjs.com/package/path|path}
+ * @author Seth Hollingsead
+ * @date 2023/03/29
+ * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
+ */
