64e1f3837ced9abb33a179562d774a18fd6118fd
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-03-30 16:07:00 -0500

    projStart: Get the basic application stoodup and basic haystacks functionality working.

---
+++ src/commands/clientCommands/testCommands.js
@@ -0,0 +1,35 @@
+/**
+ * @file testCommands.js
+ * @module testCommands
+ * @description Contains all client defined commands for execution of client test commands with various kinds of operations,
+ * specific for GUI testing, API testing, etc...
+ * @requires module:installBroker
+ * @requires module:application.command.constants
+ * @requires module:application.message.constants
+ * @requires module:application.system.constants
+ * @requires {@link https://www.npmjs.com/package/@haystacks/async|@haystacks/async}
+ * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
+ * @requires {@link https://www.npmjs.com/package/path|path}
+ * @author Seth Hollingsead
+ * @date 2023/03/30
+ * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
+ */
+
+// Internal imports
+import installBroker from '../../brokers/installBroker.js';
+import * as apc from '../../constants/application.constants.js';
+import * as app_msg from '../../constants/application.message.constants.js';
+import * as app_sys from '../../constants/application.system.constants.js';
+// External imports
+import haystacks from '@haystacks/async';
+import hayConst from '@haystacks/constants';
+import path from 'path';
+
+const {bas, msg, wrd} = hayConst;
+const baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
+// application.hay-CAF.commands.clientCommands.testCommands.
+const namespacePrefix = wrd.capplication + bas.cDot + apc.cApplicationName + bas.cDot + wrd.ccommands + bas.cDot + wrd.cclient + wrd.cCommands + bas.cDot + baseFileName + bas.cDot;
+
+export default {
+  
+}
