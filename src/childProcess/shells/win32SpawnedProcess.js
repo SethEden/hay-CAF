8996151bd0e0cbc77b6965452c8832e5fd66b2a9
SethEden <Iceversaka@hotmail.com>
2024-01-09 20:47:44 -0600

    childProcessImplementation: Fix a small type-O.

--- src/childProcess/shells/win32SpawnedProcess.js
+++ src/childProcess/shells/win32SpawnedProcess.js
@@ -95,7 +95,7 @@ export function shell(shellCommandToRun, options) {
           'start', ['C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe', '-NoExit', '-NoProfile', '-ExecutionPolicy', 'Bypass',
           'Invoke-Expression', '-Command' ], {shell: true}
         ]     
-        // Script extention
+        // Script extension
         tempFileOptions.postfix = '.ps1';
 
         // Powershell command to execute commands
