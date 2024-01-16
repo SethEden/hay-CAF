e165abcdff30ca107812750a0bc251a9753f74e8
Karl-Edward F P Jean-Mehu <karlej99@yahoo.com>
2024-01-16 17:01:10 -0500

    childProcessImplementation: removed test powershell script

--- powershellScript.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location "C:/CAFfeinated"
-testcafe firefox C:/CAFfeinated/TestBureau/SethEden/Tests/Default.test.js  --reporter html:C:/CAFfeinated/results/SethEden/reports/20240111-143939-093.html testName=Software
